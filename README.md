1c27a51374386c5d8c9f0c73680e8e04dce17780
matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
2017-05-31 22:47:50 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Hash::Unique - It's new $module
+
+# SYNOPSIS
+
+    use Hash::Unique;
+
+# DESCRIPTION
+
+Hash::Unique is ...
+
+# LICENSE
+
+Copyright (C) matsumura-taichi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
---
+++ lib/Hash/Unique.pm
@@ -0,0 +1,38 @@
+package Hash::Unique;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hash::Unique - It's new $module
+
+=head1 SYNOPSIS
+
+    use Hash::Unique;
+
+=head1 DESCRIPTION
+
+Hash::Unique is ...
+
+=head1 LICENSE
+
+Copyright (C) matsumura-taichi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+matsumura-taichi E<lt>hiroto.in.the.cromagnons@gmail.comE<gt>
+
+=cut
